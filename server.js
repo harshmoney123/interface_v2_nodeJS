85b7b9c224741021c0e2e6f8fb85e3db16bf892e
seraphywrh <ruohan.wang@vanderbilt.edu>
2018-07-22 23:47:12 -0500

    add some pages and a database

--- server.js
+++ server.js
@@ -1,18 +1,72 @@
 var express = require("express"),
     app = express(),
     binaryServer = require('binaryjs').BinaryServer,
+    bodyparser = require("body-parser"),
     wav = require('wav'),
     fs = require('fs'),
     outFile = 'speech.wav',
     spawn = require("child_process"),
-    fs = require('fs');
+    fs = require('fs'),
+    mongoose = require("mongoose");
     
-    
+app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + '/public'));
+mongoose.connect("mongodb://localhost:27017/transcriptionDB",{useNewUrlParser:true});
+
+var MySchema = new mongoose.Schema({
+    name: String,
+    password: String,
+    transcription: String,
+    date: String,
+    time: String
+});
+var Doctor = mongoose.model("Doctor", MySchema);
+var Patient = mongoose.model("Patient", MySchema);
+
+// var person = new Doctor({
+//     name: "xxx",
+//     password: "12345",
+//     transcription: "...",
+//     date: "08/04/2018",
+//     time: "14:00"
+// });
+// person.save(function(err, per){
+//     if(err){
+//         console.log("something wrong")
+//     }else{
+//         console.log("we just saved a cat to the db:")
+//         console.log(per);
+//     }
+// });
 
 app.get("/", function(req, res){
+  res.render("landing");
+});
+
+app.get("/record", function(req, res){
     res.render("client");
+});
+
+var username = "Please login";
+app.get("/profile", function(req, res){
+  res.render("profile", {username: username});
+});
+
+app.post("/profile", function(req, res){
+  var username = req.body.username;
+  Patient.find({name: username}, function(err, person){
+      if(err){
+          console.log(err);
+      }else{
+          // console.log(person[0]);
+          res.render("profile", {person: person[0]});
+      }
+  });
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
 });
 
 app.listen(3700, function(){
